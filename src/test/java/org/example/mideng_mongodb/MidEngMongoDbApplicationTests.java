d42e49ef7d65952f528a82c1e417aa63ca8ed0cd
LoganLucky365 <sebastian.profous@outlook.com>
2025-03-24 21:24:54 +0100

    Initial Commit

---
+++ src/test/java/org/example/mideng_mongodb/MidEngMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mideng_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidEngMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
